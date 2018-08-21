cda08f3df3c37df08d360abdc15eef641ca62e2a
github username <github email address>
2018-08-21 17:34:47 -0400

    working on navbar and adding route-related components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Matthew Welch, FullStack Web Developer</h1>
+        <header className="name-header">
+          <h1 className="name-header">Matthew Welch, FullStack Web Developer</h1>
+          <nav className="main-nav">
+            <ul className="main-nav-items">
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
         </header>
       </div>
     );
