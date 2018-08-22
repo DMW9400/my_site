77c2f0625256decee1d37f505143d57847bd0cbe
github username <github email address>
2018-08-21 22:59:30 -0400

    video iframes added

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -6,6 +6,10 @@ class Videos extends React.Component {
     return(
       <div className="page-div">
         Videos!
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/YGI3FfOw1PA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/T0313oMMVgM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/AimiPQc_zJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/d71bGGjwr4w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/oYQchaypUJM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     )
   }
