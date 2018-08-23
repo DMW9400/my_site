91f7be9aee5a363fec92c93273273a8763272b2f
github username <github email address>
2018-08-23 11:15:27 -0400

    finished video alignment

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,10 +5,13 @@ class Videos extends React.Component {
   render (){
     return(
       <div className="page-div">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/YGI3FfOw1PA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/T0313oMMVgM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/AimiPQc_zJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/d71bGGjwr4w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/oYQchaypUJM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <div className='iframe-div'>
+          <iframe width="560" className='app-iframe' height="315" src="https://www.youtube.com/embed/YGI3FfOw1PA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="560" className='app-iframe' height="315" src="https://www.youtube.com/embed/T0313oMMVgM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="560" className='app-iframe' height="315" src="https://www.youtube.com/embed/AimiPQc_zJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="560" className='app-iframe' height="315" src="https://www.youtube.com/embed/d71bGGjwr4w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="560" className='app-iframe' height="315" src="https://www.youtube.com/embed/oYQchaypUJM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
       </div>
     )
   }
