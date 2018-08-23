dc2f461457be21ae981a4b2c048b2c6393dfcf15
github username <github email address>
2018-08-23 11:07:12 -0400

    the journey continues

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -6,7 +6,9 @@ class Applications extends React.Component {
 
   render(){
     return(
-      <div>Applications!</div>
+      <div>
+        <a href="https://musician-finder-frontend.herokuapp.com/" target= "_blank" className="app-link"> Musician Finder App</a>
+      </div>
     )
   }
 }
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,7 +5,6 @@ class Videos extends React.Component {
   render (){
     return(
       <div className="page-div">
-        Videos!
         <iframe width="560" height="315" src="https://www.youtube.com/embed/YGI3FfOw1PA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/T0313oMMVgM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/AimiPQc_zJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/d71bGGjwr4w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
