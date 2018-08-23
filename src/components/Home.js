1786f8dae883684a7753fbdf6a8c4b8e5580b067
github username <github email address>
2018-08-23 10:39:23 -0400

    styling continues

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,10 @@ import component_styling from './component_styling.css'
 class Home extends React.Component {
   render(){
     return(
-      <div>
-        <p id="bio-p"> I have always had a love of technology and slick user interfaces. After graduating from Tufts University with a degree in English I worked several jobs in publishing, the non-profit sector, and education. After spending several years as a grant director in Memphis, Tennessee, I decided to switch up my career path and push myself towards web development. After graduating from the Flatiron School, I'm ready to take my skills to the next level.  </p>
+      <div className="bio-div">
+        <p className="bio-p"> I have always had a love of technology and slick user interfaces. After graduating from Tufts University with a degree in English I worked several jobs in publishing, the non-profit sector, and education. After spending several years as a grant director in Memphis, Tennessee, I decided to switch up my career path and push myself towards web development. After graduating from the Flatiron School, I'm ready to take my skills to the next level.  </p>
+        <a href="https://github.com/DMW9400?tab=repositories" target="_blank"><img src="http://www.joeyism.com/images/octocat.png"/></a>
+        <a href="mailto:dmw9400@gmail.com" target="_blank"><img src="https://amigostranslate.com.au/wp-content/uploads/2016/07/email-icon.png"/></a>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
       <div className="nav-div">
         <nav className="main-navbar">
           <ul className="nav-ul">
-            <li className="nav-li"><Link to="/bio">Bio</Link></li>
+            <li className="nav-li"><Link to="/home">Bio</Link></li>
             <li className="nav-li"><Link to="/apps">Apps</Link></li>
             <li className="nav-li"><Link to="/videos">Video Demos</Link></li>
             <li className="nav-li"><a href="https://soundcloud.com/matthew-welch-6/" target="_blank">SoundCloud Profile</a></li>
